e18cb3a2664de3a2a83abbf9fc75cd0e921b81f5
ZawojWeb <zawojweb@gmail.com>
2021-06-09 15:50:01 +0200

    Add Source Map

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
+let mode = "development";
+if (process.env.NODE_ENV === "production") {
+    mode = "production";
+}
 module.exports = {
-    mode: "development",
-    devtool: false,
+    mode: mode,
+    devtool: "source-map",
 
     module: {
         rules: [
