9ff9f75ae5c002698deec4846a8d1e19d10260a8
ZawojWeb <zawojweb@gmail.com>
2021-06-09 14:18:09 +0200

    Webpack server

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ const elevenShieldRecipe = {
     ironIngot: 1,
     refinedMoonstone: 4
 }
+const elevenGoutle = {
+    ...elevenShieldRecipe,
+    leather: 1,
+    refinedMoonstone: 3
+}
 console.log(elevenShieldRecipe)
+console.log(elevenGoutle)
