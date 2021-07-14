84b1e2f2ff5113caa2095ffaf82301876cfca3bf
Zawoj <zawojweb@gmail.com>
2021-07-14 21:10:56 +0200

    Add browserlistrc

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,20 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 // 'production': 'none' | 'development' | 'production'
 let mode = "development";
+let target = "web";
 if (process.env.NODE_ENV === "production") {
     mode = "production";
+    target = "browserslist";
 }
 module.exports = {
     mode: mode,
+    target: target,
     devtool: "source-map",
 
     module: {
         rules: [
             {
-                test: /\.s?css$/i,
+                test: /\.(s[ac]|c)ss$/i,
                 use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader" ,"sass-loader"]
             },
             {
